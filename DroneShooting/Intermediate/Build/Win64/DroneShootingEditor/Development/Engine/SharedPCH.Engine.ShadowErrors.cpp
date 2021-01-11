59ac9e8533e9d159576c31cc319cd2829641a318
Philipe Go <philipe.ng@protonmail.com>
2021-01-11 13:30:57 -0500

    Initial commit

---
+++ DroneShooting/Intermediate/Build/Win64/DroneShootingEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "D:/Documents/GameDesign/ue4-ml.drone.shooting/DroneShooting/Intermediate/Build/Win64/DroneShootingEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ DroneShooting/Source/DroneShooting/DroneShootingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DroneShootingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONESHOOTING_API ADroneShootingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
